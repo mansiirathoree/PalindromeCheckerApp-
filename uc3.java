86bb54002a3204751fe1bb45e05883bd334fa778
mansiirathoree <44mansirathore44@gmail.com>
2026-02-18 15:59:24 +0530

    Add files via upload

---
+++ uc3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class uc3 {
+
+    public static void main(String[] args) {
+
+        // Create Scanner object to take input from user
+        Scanner sc = new Scanner(System.in);
+
+        // Ask user to enter a string
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        // Compare original and reversed string using equals()
+        if (original.equals(reversed)) {
+            System.out.println("The string \"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + original + "\" is NOT a Palindrome.");
+        }
+
+        // Close scanner
+        sc.close();
+    }
+}
+
